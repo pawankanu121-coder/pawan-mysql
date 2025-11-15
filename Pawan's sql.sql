576d30aafaab06655f84199ab596e44bb5017fa2
pawankanu121-coder <pawankanu121@gmail.com>
2025-11-15 10:37:49 +0530

    Update Pawan's sql.

--- Pawan's sql.sql
+++ Pawan's sql.sql
@@ -1,29 +1,53 @@
--- create database saitm;
-use saitm;
--- create table dep1(
--- name varchar(30),
--- rollno int,
--- grade varchar(10),
--- marks int
--- );
 
-insert into dep1(name,rollno,grade,marks)
-value
-("bhawna",1,"A",98),
-("sakshi",2,"A",96),
-("Harshit",3,"A",91),
-("sameer",4,"D",12),
-("siddhant",5,"B",55),
-("Shrawank",6,"A",70),
-("satyam",7,"F",02);
-select name from dep1
-where name like '%A';
-select name from dep1
-where name like 'A%';
-select name from dep1
-where marks < 60
-order by marks asc;
-select name, rollno 
-from dep1
-where name like'%A'
-and rollno=1;
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: saitm
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `dep1`
+--
+
+DROP TABLE IF EXISTS `dep1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dep1` (
+  `name` varchar(30) DEFAULT NULL,
+  `rollno` int DEFAULT NULL,
+  `grade` varchar(10) DEFAULT NULL,
+  `marks` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dep1`
+--
+
+LOCK TABLES `dep1` WRITE;
+/*!40000 ALTER TABLE `dep1` DISABLE KEYS */;
+INSERT INTO `dep1` VALUES ('bhawna',1,'B',55),('sakshi',2,'A',96),('ishant',3,'C',22),('sameer',4,'D',12),('siddhant',5,'B',55),('ananya',6,'A',70),('pappu',7,'F',2),('bhawna',1,'A',98),('sakshi',2,'A',96),('Harshit',3,'A',91),('sameer',4,'D',12),('siddhant',5,'B',55),('Shrawank',6,'A',70),('satyam',7,'F',2);
+/*!40000 ALTER TABLE `dep1` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-14 19:57:42
